66093c1b8e5d444f1288fca122c4cc0b50e1e0bf
Rujan Shrestha <rujan209@gmail.com>
2022-05-22 09:33:21 +0545

    Added show/Hide pass

--- src/components/Login/LoginForm.vue
+++ src/components/Login/LoginForm.vue
@@ -17,7 +17,7 @@
     </div>
     <div class="relative z-0 w-full mb-6 group">
       <input
-        type="Password"
+        :type="passInput"
         name="Password"
         v-model="formData.password"
         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -27,8 +27,54 @@
       <label
         for="Email"
         class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-        >Password</label
+        >Password
+      </label>
+
+      <span
+        class="absolute right-0 top-3 cursor-pointer"
+        v-if="!showPass"
+        @click="showPassword(true)"
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+          />
+        </svg>
+      </span>
+      <span
+        class="absolute right-0 top-3 cursor-pointer"
+        v-else
+        @click="showPassword(false)"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+          />
+        </svg>
+      </span>
     </div>
 
     <button
@@ -54,6 +100,8 @@ export default {
     const formData = ref({ username: "admin", password: "admin" });
     const router = useRouter();
     const error = ref();
+    const showPass = ref(false);
+    const passInput = ref("Password");
 
     const checkData = () => {
       console.log(store.state);
@@ -72,10 +120,23 @@ export default {
         error.value = "Incorrect Username/ Pass";
       }
     };
+
+    const showPassword = (value) => {
+      if (value == false) {
+        showPass.value = false;
+        passInput.value = "Password";
+      } else {
+        showPass.value = true;
+        passInput.value = "Input";
+      }
+    };
     return {
       formData,
       checkData,
       error,
+      showPass,
+      passInput,
+      showPassword,
     };
   },
 };
